14fd7ec7ea1abeece43290438741bef7ec9fe779
Kashyap Sukshavasi <ksukshavasi@gmail.com>
2026-04-18 16:59:49 -0400

    feat(fonts): added hangul korean, mandarin, and japanese font support

--- src/app/mod.rs
+++ src/app/mod.rs
@@ -4,14 +4,18 @@ mod view;
 
 pub use state::{App, AppTheme, MatchState, Message, TrackMeta};
 
-use iced::Task;
+use iced::{Font, Task};
 use lucide_icons::LUCIDE_FONT_BYTES;
+
+const MULTI_LANG_BYTES: &[u8] = include_bytes!("../assets/NotoSansMultilanguage-Regular.ttf");
 
 pub fn run() -> iced::Result {
     iced::application("Rustify", App::update, App::view)
         .subscription(App::subscription)
         .theme(|app: &App| app.app_theme.to_iced_theme())
         .font(LUCIDE_FONT_BYTES)
+        .font(MULTI_LANG_BYTES)
+        .default_font(Font::with_name("NotoSans Multilanguage Regular"))
         .window_size(iced::Size::new(1280.0, 800.0))
         .run_with(|| {
             let app = App::new();
